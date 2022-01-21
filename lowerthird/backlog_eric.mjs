f3060040b8efba6f23349af2bca1b56c113f9bc8
Cosmic Effect <97470603+CosmicEffect@users.noreply.github.com>
2022-01-21 12:29:15 -0300

    Update backlog_eric.mjs

--- lowerthird/backlog_eric.mjs
+++ lowerthird/backlog_eric.mjs
@@ -14,16 +14,387 @@
 
 export function adicionarLista() {
   window.pushFile({
-    'tipo': 'EM BREVE NO CFX',
+    'tipo': 'NO BACKLOG DO CFX',
     'jogador': 'Eric',
     'dados': [
       {
         'titulo': 'Alundra',
-        'descricao': 'Playstation'
+        'descricao': 'PlayStation'
       },
       {
         'titulo': 'The Adventures of Lomax',
-        'descricao': 'Playstation'
+        'descricao': 'PlayStation'
+      {
+        'titulo': 'Shiren The Wanderer',
+        'descricao': 'Wii'
+      },
+      {
+        'titulo': 'ChainDive',
+        'descricao': 'PlayStation 2'
+      },
+      {
+        'titulo': 'Karnov',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Magician',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Operation Logic Bomb',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Brandish',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Charge\'n Blast',
+        'descricao': 'Dreamcast'
+      },
+      {
+        'titulo': 'Mass Destruction',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'Rusty',
+        'descricao': 'MS-DOS'
+      },
+      {
+        'titulo': 'Holy Diver',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Lords Of Thunder',
+        'descricao': 'Sega CD'
+      },
+      {
+        'titulo': 'Legacy Of Wizard',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Star Ocean',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': '3x3 Eyes: Juuma Houkan',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Contra III: The Alien Wars',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Spriggan Powered',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Snoopy And The Red Baron',
+        'descricao': 'PlayStation 2'
+      },
+      {
+        'titulo': 'Mazin Saga: Mutant Fighter',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Kabuki Quantum Fighter',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Metal Black',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'Thunder Force V',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'Bulk Slash',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'Nightshade',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Psycho Dream',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Shinobi X',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'U-four-ia: The Saga',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Technoclash',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Battletech',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Jim Power',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Nuclear Strike 64',
+        'descricao': 'Nintendo 64'
+      },
+      {
+        'titulo': 'Valkiria Chronicles',
+        'descricao': 'PlayStation 3'
+      },
+      {
+        'titulo': 'Castlevania',
+        'descricao': 'Nintendo 64'
+      },
+      {
+        'titulo': 'Keio Flying Squadron',
+        'descricao': 'Sega CD'
+      },
+      {
+        'titulo': 'Keio Flying Squadron 2',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'Rygar',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Xexyz',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'The Guardian Legend',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Predator 2',
+        'descricao': 'Master System'
+      },
+      {
+        'titulo': 'Time Trax',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Devastator',
+        'descricao': 'Sega CD'
+      },
+      {
+        'titulo': 'Android Assault: The Revenge Of Bari-Arm',
+        'descricao': 'Sega CD'
+      },
+      {
+        'titulo': 'Wolfchild',
+        'descricao': 'Sega CD'
+      },
+      {
+        'titulo': 'Flink',
+        'descricao': 'Sega CD'
+      },
+      {
+        'titulo': 'Willow',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Black',
+        'descricao': 'PlayStation 2'
+      },
+      {
+        'titulo': 'Alcahest',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Return Fire',
+        'descricao': '3DO'
+      },
+      {
+        'titulo': 'Mischief Makers',
+        'descricao': 'Nintendo 64'
+      },
+      {
+        'titulo': 'Faxanadu',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Ironclad',
+        'descricao': 'Neo Geo CD'
+      },
+      {
+        'titulo': 'Alienation',
+        'descricao': 'PlayStation 4'
+      },
+      {
+        'titulo': 'Salt And Sanctuary',
+        'descricao': 'PlayStation 4'
+      },
+      {
+        'titulo': 'Metal Storn',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Princess Crown',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'Virtual On: Cyber Troopers',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'Red Zone',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Colony Wars',
+        'descricao': 'PlayStation'
+      },
+      {
+        'titulo': 'E.V.O.: Search For Eden',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Eternal Sonata',
+        'descricao': 'Xbox 360'
+      },
+      {
+        'titulo': 'Blue\'s Journey',
+        'descricao': 'Neo Geo CD'
+      },
+      {
+        'titulo': 'Lost Kingdoms',
+        'descricao': 'GameCube'
+      },
+      {
+        'titulo': 'Sin And Punishment',
+        'descricao': 'Nintendo 64'
+      },
+      {
+        'titulo': 'Valkyrie Profile',
+        'descricao': 'PlayStation'
+      },
+      {
+        'titulo': 'King\'s Knight',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Star Tropics',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Sengoku 2',
+        'descricao': 'Neo Geo CD'
+      },
+      {
+        'titulo': 'Grandia',
+        'descricao': 'PlayStation'
+      },
+      {
+        'titulo': 'Atomic Runner',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Alien Soldier',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Maze Hunter 3D',
+        'descricao': 'Master System'
+      },
+      {
+        'titulo': 'Glory Of Heracles IV',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Breakdown',
+        'descricao': 'Xbox'
+      },
+      {
+        'titulo': 'Darkwatch',
+        'descricao': 'PlayStation 2'
+      },
+      {
+        'titulo': 'Frame Gride',
+        'descricao': 'Dreamcast'
+      },
+      {
+        'titulo': 'Xak: The Art Of Visual Stage',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Tunnel B1',
+        'descricao': 'Saturn'
+      },
+      {
+        'titulo': 'Lost In Shadow',
+        'descricao': 'Wii'
+      },
+      {
+        'titulo': 'Genocide 2',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Little Samson',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Gimmick',
+        'descricao': 'Nintendo'
+      },
+      {
+        'titulo': 'Space Invaders 91',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Psi-Ops: The Mindgate Conspiracy',
+        'descricao': 'PlayStation 2'
+      },
+      {
+        'titulo': 'Dead Tonosama Appare Ichiban',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Dezaemon',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Marchen Adventure Cotton 100%',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Pop\'n Twinbee',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Magical Pop\'n',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Kishin Douji Zenki: Tenchi Meidou',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Super Ninja-kun',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Kaizou Choujin Schbibinman Zero',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'Front Mission: Gun Hazard',
+        'descricao': 'Super Nintendo'
+      },
+      {
+        'titulo': 'King Colossus',
+        'descricao': 'Mega Drive'
+      },
+      {
+        'titulo': 'Code Name: Viper',
+        'descricao': 'Nintendo'
       }
     ]
   });

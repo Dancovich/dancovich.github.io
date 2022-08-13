320a1f17e18405de9f9387626d264c4df0daef7b
Dancovich <dcviana@gmail.com>
2022-08-13 10:21:55 -0300

    Adicionadas dicas de revista

--- lowerthird/dicas_da_revista.mjs
+++ lowerthird/dicas_da_revista.mjs
@@ -22,48 +22,64 @@ export function adicionarLista() {
         'descricao': 'Ação Games Nº 3'
       },
       {
-        'titulo': 'Sai de baixo! Pintou sujeira: como é pentelho este velhinho.',
+        'titulo': 'Sai de baixo! Pintou sujeira: como é pentelho este velhinho',
         'descricao': 'Ação Games Nº 41'
       },
       {
-        'titulo': 'A chave da porta que você pegou lá atrás serve para abrir portas que o levarão a outros locais da fase.',
+        'titulo': 'A chave da porta que você pegou lá atrás serve para abrir portas que o levarão a outros locais da fase',
         'descricao': 'Ação Games Nº 3'
       },
       {
-        'titulo': 'Para obter o continue nesse game, espere pela tela de game over.',
+        'titulo': 'Para obter o continue nesse game, espere pela tela de game over',
         'descricao': 'Ação Games Nº 3'
       },
       {
-        'titulo': 'Não gaste magias à toa em Biggs e Wedge.',
+        'titulo': 'Não gaste magias à toa em Biggs e Wedge',
         'descricao': 'Super Game Power Nº 62'
       },
       {
-        'titulo': 'Para conseguir vencer o computador em qualquer nível de dificuldade não é preciso nenhum esforço sobrenatural.',
+        'titulo': 'Para conseguir vencer o computador em qualquer nível de dificuldade não é preciso nenhum esforço sobrenatural',
         'descricao': 'Super Game Power Nº 3'
       },
       {
-        'titulo': 'Quando aparecer um cara que ataca de perto, não vacile. Atire no ato.',
+        'titulo': 'Quando aparecer um cara que ataca de perto, não vacile. Atire no ato',
         'descricao': 'Super Game Power Nº 87'
       },
       {
-        'titulo': 'A munição é ilimitada, por isso fique sempre atirando enquanto explora novos territórios.',
+        'titulo': 'A munição é ilimitada, por isso fique sempre atirando enquanto explora novos territórios',
         'descricao': 'Super Game Power Nº 42'
       },
       {
-        'titulo': 'Quanto mais poderoso você for, mais fácil será derrotar seus inimigos.',
+        'titulo': 'Quanto mais poderoso você for, mais fácil será derrotar seus inimigos',
         'descricao': 'Guia Games, Abril 92'
       },
       {
-        'titulo': 'No túnel do planeta verde não pare de atirar e soltar mísseis, pois assim você derrotará os inimigos ao aparecerem na tela.',
+        'titulo': 'No túnel do planeta verde não pare de atirar e soltar mísseis, pois assim você derrotará os inimigos ao aparecerem na tela',
         'descricao': 'Videogame Nº 4'
       },
       {
-        'titulo': 'Para pegar a manha, aprenda a mover-se e atirar com precisão.',
+        'titulo': 'Para pegar a manha, aprenda a mover-se e atirar com precisão',
         'descricao': 'Super Game Power Nº 60'
       },
       {
-        'titulo': 'Mate a maioria dos inimigos apenas apertando o botão de ataque.',
+        'titulo': 'Mate a maioria dos inimigos apenas apertando o botão de ataque',
         'descricao': 'Super Game Power Nº 60'
+      },
+      {
+        'titulo': 'Consulte o mapa sempre que possível para não se perder no cenário',
+        'descricao': 'Revista Playstation Nº 9'
+      },
+      {
+        'titulo': 'Procure falar com todos durante o jogo, pois eles dão informações muito importantes',
+        'descricao': 'Super Game Power Nº 3'
+      },
+      {
+        'titulo': 'As vezes o jogo indica para apertar os botões (A, B ou C). Fique esperto!',
+        'descricao': 'Super Game Power Nº 3'
+      },
+      {
+        'titulo': 'Verifique tudo a fim de conseguir itens e dicas',
+        'descricao': 'Super Game Power Nº 3'
       }
     ]
   });

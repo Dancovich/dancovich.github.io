561cf4d774c74a5039feb6e29573225b5ed80793
Dancovich <dcviana@gmail.com>
2024-08-09 13:39:38 -0300

    Ajustado tempo de aparecimento

--- lowerthird/config.js
+++ lowerthird/config.js
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // Configurações de tempo são em segundos
-//var TEMPO_MINIMO = 300;
-//var TEMPO_MAXIMO = 1800;
-var TEMPO_MINIMO = 2;
-var TEMPO_MAXIMO = 3;
+var TEMPO_MINIMO = 300;
+var TEMPO_MAXIMO = 1800;

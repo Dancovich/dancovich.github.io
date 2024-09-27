e0de70fbd51813dc0b29cf852f607223a6c13190
Cosmic Effect <97470603+CosmicEffect@users.noreply.github.com>
2024-09-27 13:38:48 -0300

    Update contador_giger.mjs

--- lowerthird/contador_giger.mjs
+++ lowerthird/contador_giger.mjs
@@ -4,68 +4,120 @@ export function adicionarLista() {
         'jogador': 'Eric',
         'dados': [
             {
-                'titulo': 'Cyber-Cop - Mega Drive',
+                'titulo': 'Cyber-Cop *Mega Drive)',
                 'descricao': 'Embrião subnível 3'
             },
             {
-                'titulo': 'Perkele - NES',
+                'titulo': 'Perkele (NES)',
                 'descricao': 'Cutscenes antes do último chefe'
             },
             {
-                'titulo': 'F-Zero X - Nintendo 64',
+                'titulo': 'F-Zero X (Nintendo 64)',
                 'descricao': 'Personagem "Pico"'
             },
             {
-                'titulo': 'Ashguine - MSX',
+                'titulo': 'Ashguine (MSX)',
                 'descricao': '2 inimigos no começo com cabeça de Alien'
             },
             {
-                'titulo': 'Lionheart - Amiga',
+                'titulo': 'Lionheart (Amiga)',
                 'descricao': 'Fase secreta tem cenário Gigger'
             },
             {
-                'titulo': 'Moons of Darsalon - PC',
+                'titulo': 'Moons of Darsalon (PC)',
                 'descricao': 'Árvore gigante Alien'
             },
             {
-                'titulo': 'Splatterhouse 3 - Mega Drive',
+                'titulo': 'Splatterhouse 3 (Mega Drive)',
                 'descricao': '5ª fase tem cenário de fundo biomecânico'
             },
             {
-                'titulo': 'Ecco the Dolphin - Sega CD',
+                'titulo': 'Ecco The Dolphin (Sega CD)',
                 'descricao': '29ª fase tem fundo biomecânico e Alien na 30ª fase'
             },
             {
-                'titulo': 'Uicho To Tora - Super NES',
+                'titulo': 'Uchio To Tora (Super NES)',
                 'descricao': 'Alien verde em cima de avião'
             },
             {
-                'titulo': 'Mystic Defender - Mega Drive',
+                'titulo': 'Mystic Defender (Mega Drive)',
                 'descricao': 'Alien cuspindo fogo na 3ª fase'
             },
             {
-                'titulo': 'Legendary Axe 2 - PC Engine',
-                'descricao': 'Giger sutil na  6ª fase'
+                'titulo': 'Legendary Axe 2 (PC Engine)',
+                'descricao': 'Giger sutil no fundo 6ª fase'
             },
             {
-                'titulo': 'Phoenix 3 - 3DO',
+                'titulo': 'Phoenix 3 (3DO)',
                 'descricao': 'Alien "canalha"'
             },
             {
-                'titulo': 'Aliens: Neoplasma - ZX Spectrum Next',
+                'titulo': 'Aliens: Neoplasma (ZX Spectrum Next)',
                 'descricao': 'Aliens o tempo todo'
             },
             {
-                'titulo': 'Devastator - Sega CD',
+                'titulo': 'Devastator (Sega CD)',
                 'descricao': 'Giger no background da 2ª fase'
             },
             {
-                'titulo': 'Flashback Legends - Gameboy Advance',
+                'titulo': 'Flashback Legends (GBA)',
                 'descricao': 'Giger na textura de piso de várias fases'
             },
             {
-                'titulo': 'Rubicon - Amiga',
+                'titulo': 'Rubicon (Amiga)',
                 'descricao': 'Giger na 6º fase, ovo e facehugger'
+            },
+            {
+                'titulo': 'Kabuki Quantum Fighter (NES)',
+                'descricao': 'Facehugger e Alien na 2º fase'
+            },
+            {
+                'titulo': 'Submarine Attack (Master System)',
+                'descricao': 'Alien dentro da redoma'
+            },
+            {
+                'titulo': 'Macross Scramble Valkyrie (Super NES)',
+                'descricao': '3º forma do último chefe'
+            },
+            {
+                'titulo': 'Alien World (Amiga)',
+                'descricao': 'Personagem principal'
+            },
+            {
+                'titulo': 'Mega Turrican (Mega Drive)',
+                'descricao': ' Cenário da quarta fase'
+            },
+            {
+                'titulo': 'Castlevania SoTN (PS1)',
+                'descricao': ' Inimigo da catacumba lembra xeno'
+            },
+            {
+                'titulo': 'Bio Hazard Battle (Mega Drive)',
+                'descricao': 'Chefe da 3ª fase'
+            },
+            {
+                'titulo': 'Alien Soldier (Mega Drive)',
+                'descricao': 'Chefe Sniper Honeyviper'
+            },
+            {
+                'titulo': 'Alien Breed (MS-DOS)',
+                'descricao': 'Tela inicial e chefe final'
+            },
+            {
+                'titulo': 'Dark Seed (MS-DOS)',
+                'descricao': 'Por onde começar...'
+            },
+            {
+                'titulo': 'XenoCrisis (Mega Drive)',
+                'descricao': 'Pelo jogo todo'
+            },
+            {
+                'titulo': 'Soukyugurentai Okuyo (Saturn)',
+                'descricao': 'Chefe da 4ª fase'
+            },
+            {
+                'titulo': 'Septerra Core (PC)',
+                'descricao': 'Os Underlost raça do personagem Badu'
             }
         ]
     });

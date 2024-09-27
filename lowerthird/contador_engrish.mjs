e54dbf3c19b2219cf1f289323dbe4c7522769cb9
Cosmic Effect <97470603+CosmicEffect@users.noreply.github.com>
2024-09-27 12:52:09 -0300

    Update contador_engrish.mjs

--- lowerthird/contador_engrish.mjs
+++ lowerthird/contador_engrish.mjs
@@ -4,8 +4,20 @@ export function adicionarLista() {
         'jogador': 'Eric',
         'dados': [
             {
-                'titulo': 'Lorem Ipsum',
-                'descricao': 'No local Lorem Ipsum'
+                'titulo': 'Devastator (Sega CD)',
+                'descricao': 'Créditos finais lotados de engrish'
+            },
+            {
+                'titulo': 'Border Down (Dreancast)',
+                'descricao': 'Engrish o jogo inteiro'
+            },
+            {
+                'titulo': 'Macross Scramble Valkyrie (Super NES)',
+                'descricao': 'Tela de options chama-se CONFIGLATION'
+            },
+            {
+                'titulo': 'Dino Land (Mega Drive)',
+                'descricao': 'Options apresenta dificuldade BIGINNER'
             }
         ]
     });

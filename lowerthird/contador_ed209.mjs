a5ae5794f606fef37ed03298bc2704a79c9d03f8
Cosmic Effect <97470603+CosmicEffect@users.noreply.github.com>
2024-09-27 12:35:05 -0300

    Update contador_ed209.mjs

--- lowerthird/contador_ed209.mjs
+++ lowerthird/contador_ed209.mjs
@@ -4,8 +4,72 @@ export function adicionarLista() {
         'jogador': 'Eric',
         'dados': [
             {
-                'titulo': 'Lorem Ipsum',
-                'descricao': 'No local Lorem Ipsum'
+                'titulo': 'Metal Warriors (Super NES)',
+                'descricao': 'ED na quinta fase'
+            },
+            {
+                'titulo': 'Life On Earth (Mega Drive)',
+                'descricao': 'Primeiro chefe'
+            },
+            {
+                'titulo': 'Future Cop LAPD (PS1)',
+                'descricao': 'ED é inimigo básico'
+            },
+            {
+                'titulo': 'Metal Black (Saturn)',
+                'descricao': 'Pequeno robô verde da quarta fase'
+            },
+            {
+                'titulo': 'Cave Story (PC)',
+                'descricao': 'Um chefe com pés idênticos ao de ED'
+            },
+            {
+                'titulo': 'Metal Morph (Super NES)',
+                'descricao': 'Inimigo normal da quarta fase, ED-209 pobrinho'
+            },
+            {
+                'titulo': 'Stargunner (MS-DOS)',
+                'descricao': 'ED no solo da quarta fase'
+            },
+            {
+                'titulo': 'Astro Marine Corps (Amiga)',
+                'descricao': 'ED na terceira fase'
+            },
+            {
+                'titulo': 'Power Blade (NES)',
+                'descricao': 'ED inimigo normal da primeira fase'
+            },
+            {
+                'titulo': 'Rubicon (Amiga)',
+                'descricao': 'ED é minichefe da quarta fase'
+            },
+            {
+                'titulo': 'Dark Angel (Amiga)',
+                'descricao': 'ED na segunda fase'
+            },
+            {
+                'titulo': 'Mega Turrican (Mega Drive)',
+                'descricao': 'Chefe da penúltima fase'
+            },
+            {
+                'titulo': 'Axelay (Super NES)',
+                'descricao': 'Chefe da segunda fase'
+            },
+            {
+                'titulo': 'Soul Star (Sega CD)',
+                'descricao': 'Agressor forma Combat Walker é um ED'
+            },
+            {
+                'titulo': 'Sol Feace (Sega CD)',
+                'descricao': 'Sub-chefe da segunda fase'
+            },
+            {
+                'titulo': 'Mechwarrior 3050 (Super NES)',
+                'descricao': 'O robô que você controla'
+            },
+            {
+                'titulo': 'Soukyugurentai Okuyo (Saturn)',
+                'descricao': 'EDs na quinta fase'
             }
         ]
     });

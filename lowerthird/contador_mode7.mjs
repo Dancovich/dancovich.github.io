562dcdc2f479499bf677a8db2cf17460f546d358
Cosmic Effect <97470603+CosmicEffect@users.noreply.github.com>
2024-09-27 13:50:23 -0300

    Update contador_mode7.mjs

--- lowerthird/contador_mode7.mjs
+++ lowerthird/contador_mode7.mjs
@@ -4,48 +4,56 @@ export function adicionarLista() {
         'jogador': 'Eric',
         'dados': [
             {
-                'titulo': 'TMNT2: The Arcade Game - NES',
+                'titulo': 'TMNT2: The Arcade Game (NES)',
                 'descricao': 'Cutscene 7ª fase tem tartaruga "escalonando"'
             },
             {
-                'titulo': 'Ballz - Mega Drive',
+                'titulo': 'Ballz (Mega Drive)',
                 'descricao': 'Lutadores escalonam'
             },
             {
-                'titulo': 'TMNT3: The Manhattan Project - NES',
+                'titulo': 'TMNT3: The Manhattan Project (NES)',
                 'descricao': 'Fase 2 tartarugas "escalonam"'
             },
             {
-                'titulo': 'Unreal - Amiga',
+                'titulo': 'Unreal (Amiga)',
                 'descricao': 'Escala nas fases 3D'
             },
             {
-                'titulo': 'Lorna - Amiga',
+                'titulo': 'Lorna (Amiga)',
                 'descricao': 'Fase de moto voadora com escala'
             },
             {
-                'titulo': 'Uchi To Tora - Super NES',
+                'titulo': 'Uchio To Tora (Super NES)',
                 'descricao': '5ª fase rotação/escala de avião'
             },
             {
-                'titulo': 'Robot Tank - Atari 2600',
+                'titulo': 'Robot Tank (Atari 2600)',
                 'descricao': '"Escala" no tanque inimigo'
             },
             {
-                'titulo': 'Devastator - Sega CD',
+                'titulo': 'Devastator (Sega CD)',
                 'descricao': 'Chefe 2ª fase escalona'
             },
             {
-                'titulo': 'Indiana Jones Greatest Adventures - Super NES',
+                'titulo': 'Indiana Jones Greatest Adventures (Super NES)',
                 'descricao': 'Fase do avião, mina e barquinho com escala'
             },
             {
-                'titulo': 'Arcus Odyssey - Mega Drive',
+                'titulo': 'Arcus Odyssey (Mega Drive)',
                 'descricao': 'Game over com rotação e escala'
             },
             {
-                'titulo': 'The Adventures of Lomax - PlayStation 1',
+                'titulo': 'The Adventures of Lomax (PlayStation 1)',
                 'descricao': 'Escala e rotação em todo lugar'
+            },
+            {
+                'titulo': 'Metal Morph (Super NES)',
+                'descricao': 'Escala e rotação na abertura "Origin"'
+            },
+            {
+                'titulo': 'Macross Scrambled Valkyrie (Super NES)',
+                'descricao': 'Escala e rotação desde abertura até o fim'
             }
         ]
     });

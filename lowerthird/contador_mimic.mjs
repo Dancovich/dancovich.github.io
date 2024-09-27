c8c03943c4f1ee1a2d0d5d94a240db7b09ba6cf5
Cosmic Effect <97470603+CosmicEffect@users.noreply.github.com>
2024-09-27 13:46:54 -0300

    Update contador_mimic.mjs

--- lowerthird/contador_mimic.mjs
+++ lowerthird/contador_mimic.mjs
@@ -4,12 +4,28 @@ export function adicionarLista() {
         'jogador': 'Eric',
         'dados': [
             {
-                'titulo': 'Zenki Battle Raiden - Super NES',
+                'titulo': 'Zenki Battle Raiden (Super NES)',
                 'descricao': 'Tumba se torna uma aranha'
             },
             {
-                'titulo': 'Devilish - Mega Drive',
+                'titulo': 'Devilish (Mega Drive)',
                 'descricao': 'Mimico na 4ª fase'
+            },
+            {
+                'titulo': 'Alex Kidd In Miracle World (Master System)',
+                'descricao': 'Bloquinhos caveira'
+            },
+            {
+                'titulo': 'Super Mario RPG (Super NES)',
+                'descricao': 'Kero Sewers'
+            },
+            {
+                'titulo': 'Final Fantasy IX (PlayStation 1)',
+                'descricao': 'Mímicos em Burmecia'
+            },
+            {
+                'titulo': 'Dark Souls (PlayStation 3)',
+                'descricao': 'Alguns baús'
             }
         ]
     });

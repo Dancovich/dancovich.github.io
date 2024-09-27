7c436bfd41b08d4faba7abf42a27b0d10b06f772
Cosmic Effect <97470603+CosmicEffect@users.noreply.github.com>
2024-09-27 12:10:21 -0300

    Update contador_amiga_vespa.mjs

--- lowerthird/contador_amiga_vespa.mjs
+++ lowerthird/contador_amiga_vespa.mjs
@@ -4,23 +4,23 @@ export function adicionarLista() {
         'jogador': 'Eric',
         'dados': [
             {
-                'titulo': 'The Plague - Amiga',
+                'titulo': 'The Plague (Amiga)',
                 'descricao': 'Vespas comuns e chefe final'
             },
             {
-                'titulo': 'Dark Angel - Amiga',
+                'titulo': 'Dark Angel (Amiga)',
                 'descricao': 'Vespa robô gigante no fundo da 1ª fase'
             },
             {
-                'titulo': 'Agony - Amiga',
+                'titulo': 'Agony (Amiga)',
                 'descricao': 'Chefe da segunda fase montado em uma "vespa"'
             },
             {
-                'titulo': 'Metal Mutant - Amiga',
+                'titulo': 'Metal Mutant (Amiga)',
                 'descricao': 'Vespa robótica controlada remotamente'
             },
             {
-                'titulo': 'Entity - Amiga',
+                'titulo': 'Entity (Amiga)',
                 'descricao': 'Vespa na 2ª fase'
             }
         ]

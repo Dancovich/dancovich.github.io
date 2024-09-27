cb32ed93adbf32594f4b040d156dd3dfa46c5188
Cosmic Effect <97470603+CosmicEffect@users.noreply.github.com>
2024-09-27 11:56:15 -0300

    Update contador_barata.mjs

--- lowerthird/contador_barata.mjs
+++ lowerthird/contador_barata.mjs
@@ -4,16 +4,20 @@ export function adicionarLista() {
         'jogador': 'Eric',
         'dados': [
             {
-                'titulo': 'Prince of Persia, The Sands of Time - PS3',
+                'titulo': 'Prince of Persia The Sands of Time (PS3)',
                 'descricao': 'Baratas voadoras'
             },
             {
-                'titulo': 'Arcus Odyssey - Mega Drive',
+                'titulo': 'Arcus Odyssey (Mega Drive)',
                 'descricao': 'Barata na 4ª fase'
             },
             {
-                'titulo': 'Pier Solar - Mega Drive',
+                'titulo': 'Pier Solar (Mega Drive)',
                 'descricao': 'Baratas em cavernas'
+            },
+            {
+                'titulo': 'Kabuki Quantum Fighter (Nintendo)',
+                'descricao': 'Barata meio alien por sinal'
             }
         ]
     });
